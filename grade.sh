e27e8f642970b79af1861f6acfd7191e551bf969
oowretep <30489739+oowretep@users.noreply.github.com>
2024-02-15 09:31:47 -0800

    grade script and gitignore

--- grade.sh
+++ grade.sh
@@ -5,9 +5,39 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission 2> git_output.txt
 echo 'Finished cloning'
 
+if ! [[ -f student-submission/ListExamples.java ]]
+then
+    echo "Missing student-submission/ListExamples.java"
+    exit
+fi
+
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]] 
+then
+    echo "The program failed to compile. See error above"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+
+if [[ $lastline = *OK* ]]
+then
+    echo "Grade: 100%"
+else
+    tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+    failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+    successes=$((tests - failures))
+    echo "Grade: $successes / $tests"
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
